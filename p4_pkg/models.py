eec8e342e99943562940c049a14082b56b4e8bb5
Nikolay Filippov <nph@rambler.ru>
2020-02-29 21:28:54 +0300

    modified models.py, added all relationships. Added base.html to templates.

--- p4_pkg/models.py
+++ p4_pkg/models.py
@@ -10,24 +10,34 @@ class User(db.Model):
     mail = db.Column(db.String, nullable=False)
     password = db.Column(db.String, nullable=False)
     address = db.Column(db.String, nullable=False)
-    orders = db.relationship('_', back_populates='_')
+    order_id = db.Column(db.Integer, db.ForeignKey('orders.id'))
+    orders = db.relationship('Order', back_populates='users')
 
-class Group(db.Model):
-    __tablename__ = 'groups'
+meals_orders_association = db.Table('meals_orders',
+                                    db.Column('meal_id', db.Integer, db.ForeginKey('meals.id')),
+                                    db.Column('order_id', db.Integer, db.ForeignKey('orders.id')))
+class Meal(db.Model):
+    __tablename__ = 'meals'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, unique=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
+    price = db.Column(db.Decimal, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    picture = db.Column(db.String, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+    category = db.relationship('Category', back_populates='meals')
+    orders = db.relationship('Order', secondary=meals_orders_association, back_populates='meals')
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    meals = db.relationsip('Meal', back_populates='category')
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    amount = db.Column(db.Decimal, nullable=False)
     status = db.Column(db.String, nullable=False)
-    course = db.Column(db.String, nullable=False)
-    start = db.Column(db.Date, nullable=False)
-    applications = db.relationship('Applicant', back_populates='group')
-
-class Applicant(db.Model):
-    __tablename__ = 'applicants'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
-    phone = db.Column(db.String, nullable=False)
-    mail = db.Column(db.String, nullable=False)
-    status = db.Column(db.String,  nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
-    group = db.relationship('Group', back_populates='applications')
-
+    meals = db.relationship('Meal', secondary=meals_orders_association, back_populates='orders')
+    users = db.relationship('User', back_populates='orders')
